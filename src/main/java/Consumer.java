c318d72c2586b17507179c0b413bc658d2f40efe
Miguel Mejia <miguelmejia92@outlook.com>
2021-06-30 22:39:11 -0500

    Create a message consumer

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -29,6 +29,19 @@ public class Consumer {
                 // Create destination
                 Destination destination = session.createQueue("message.queue");
 
+                // Create message consumer from the sessionj tp the Topic or Queue
+                MessageConsumer consumer = session.createConsumer(destination);
+
+                // Wait form a message
+                Message message = consumer.receive(1000);
+
+                if(message instanceof TextMessage) {
+                    TextMessage textMessage = (TextMessage) message;
+                    String text = textMessage.getText();
+                    System.out.println("Received: " + text);
+                } else {
+                    System.out.println("Received: " + message);
+                }
 
             } catch (Exception e) {
 
